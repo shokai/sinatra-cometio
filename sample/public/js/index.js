ba4972163b251281c58dc810e73d954dcb740f8a
Sho Hashimoto <hashimoto@shokai.org>
2012-11-10 12:08:23 +0900

    ' -> "

--- sample/public/js/index.js
+++ sample/public/js/index.js
@@ -1,21 +1,21 @@
 var io = new CometIO().connect();
 
 io.on("chat", function(data){
-  var m = $('<li>').text(data.name + ' : ' +data.message);
-  $('#chat #timeline').prepend(m);
+  var m = $("<li>").text(data.name + " : " +data.message);
+  $("#chat #timeline").prepend(m);
 });
 
 $(function(){
-  $('#chat #btn_send').click(post);
-  $('#chat #message').keydown(function(e){
+  $("#chat #btn_send").click(post);
+  $("#chat #message").keydown(function(e){
     if(e.keyCode == 13) post();
   });
 });
 
 var post = function(){
-  var name = $('#chat #name').val();
-  var message = $('#chat #message').val();
+  var name = $("#chat #name").val();
+  var message = $("#chat #message").val();
   if(message.length < 1) return;
   io.push("chat", {name: name, message: message});
-  $('#chat #message').val('');
+  $("#chat #message").val("");
 };
