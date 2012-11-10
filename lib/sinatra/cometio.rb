47ccb785340874179640223cc3c76a0d2fdf06f6
Sho Hashimoto <hashimoto@shokai.org>
2012-11-10 22:03:32 +0900

    load sinatra-cometio.rb in gem

--- lib/sinatra/cometio.rb
+++ lib/sinatra/cometio.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'event_emitter'
 require 'sinatra/streaming'
 require File.expand_path 'application', File.dirname(__FILE__)
+require File.expand_path '../sinatra-cometio', File.dirname(__FILE__)
 
 class CometIO
   def self.channel
