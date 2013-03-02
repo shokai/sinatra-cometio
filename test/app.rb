834d3f52c50571e7d11f458b73deaf41557032ab
Sho Hashimoto <hashimoto@shokai.org>
2013-03-03 00:37:18 +0900

    add App starter in test  #7

---
+++ test/app.rb
@@ -0,0 +1,57 @@
+class App
+
+  def self.running
+    @running ? true : false
+  end
+
+  def self.port
+    ENV['PORT'] || 5000
+  end
+
+  def self.cometio_url
+    "http://localhost:#{port}/cometio/io"
+  end
+
+  def self.pid_file
+    ENV['PID_FILE'] || "/tmp/sinatra-cometio-test-pid"
+  end
+
+  def self.app_dir
+    File.expand_path 'app', File.dirname(__FILE__)
+  end
+
+  def self.pid
+    return unless @running
+    File.open(pid_file) do |f|
+      pid = f.gets.strip.to_i
+      return pid if pid > 0
+    end
+    return
+  end
+
+  def self.start
+    return if @running
+    File.delete pid_file if File.exists? pid_file
+    Thread.new do
+      IO::popen "cd #{app_dir} && PID_FILE=#{pid_file} rackup config.ru -p #{port} > /dev/null 2>&1"
+    end
+    @running = true
+    100.times do
+      if File.exists? pid_file
+        sleep 1
+        return true
+      end
+      sleep 0.1
+    end
+    @running = false
+    return false
+  end
+
+  def self.stop
+    return unless @running
+    system "kill #{pid}"
+    File.delete pid_file if File.exists? pid_file
+    @running = false
+  end
+
+end
--- test/test_cometio.rb
+++ test/test_cometio.rb
@@ -3,22 +3,12 @@ require File.expand_path 'test_helper', File.dirname(__FILE__)
 class TestCometio < MiniTest::Unit::TestCase
 
   def setup
-    api_url = "http://localhost:5000/cometio/io"
-    app_dir = File.expand_path 'app', File.dirname(__FILE__)
-    pid_file = "/tmp/sinatra-cometio-test-pid"
-    File.delete pid_file if File.exists? pid_file
-    Thread.new do
-      IO::popen "cd #{app_dir} && PID_FILE=#{pid_file} rackup config.ru -p 5000"
-    end
-    100.times do
-      break if File.exists? pid_file
-      sleep 0.1
-    end
-    File.open(pid_file) do |f|
-      @pid = f.gets.strip
-    end
-    sleep 3
-    @client = CometIO::Client.new(api_url).connect
+    App.start
+    @client = CometIO::Client.new(App.cometio_url).connect
+  end
+
+  def teardown
+    App.stop
   end
 
   def test_echo_all
@@ -39,7 +29,6 @@ class TestCometio < MiniTest::Unit::TestCase
     assert res != nil, 'server not respond'
     assert res["time"] == post_data[:time]
     assert res["msg"] == post_data[:msg]
-    system "kill #{@pid}"
   end
 
 end
